8d6d333772a4411c2c08967796e2cf1eaa1c5b11
Bhone Sithu <sithulayy@gmail.com>
2023-06-15 22:43:48 +0630

    track notistafflist

---
+++ pages/staff/notiListStaff.php
@@ -0,0 +1,76 @@
+<?php
+include('../connect.php');
+include('staffhead.php');
+
+if (isset($_POST['btnadd'])) {
+    echo "<script>window.location='customerservice.php'</script>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script type="text/javascript" src="js/jquery-3.1.1.slim.min.js"></script>
+    <script type="text/javascript" src="DataTables/datatables.min.js"></script>
+</head>
+<body>
+
+<div id="body-section">
+    <?php
+    $c_List = "SELECT * 
+			 FROM noti
+			 ";
+    $c_ret = mysqli_query($connection, $c_List);
+    $c_count = mysqli_num_rows($c_ret);
+
+    if ($c_count < 1) {
+        echo '<h1 class="form-title mt-5" style="color:var(--theme-red);">No Notification Found.</p>';
+        echo "<script>window.location='adminmain.php'</script>";
+
+    } else {
+        ?>
+        <h1 class="form-title mt-5">Notifications</h1>
+        <div class="table-container ">
+            <table id="tableid" class="table">
+                <thead>
+                <tr>
+                    <!-- <th>#</th> -->
+                    <th>Notification</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                for ($i = 0; $i < $c_count; $i++) {
+                    $rows = mysqli_fetch_array($c_ret);
+                    //print_r($rows);
+
+                    $noti = $rows['log'];
+
+                    echo "<tr>";
+                    // echo "<td>" . ($i + 1) ."</td>";
+                    echo "<td>$noti</td>";
+                    echo "</tr>";
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+
+        <?php
+    }
+    ?>
+</div>
+<?php
+include('../footer.php'); ?>
+
+<script>
+    $(document).ready(function () {
+        $('#tableid').DataTable();
+    });
+</script>
+<!--<footer></footer>-->
+</body>
+</html>
