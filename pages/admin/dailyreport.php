7e342df84bea4c9f857842a14cd21755a6fc2cd1
Bhone Sithu <sithulayy@gmail.com>
2023-06-20 14:06:00 +0630

    daily report added

--- pages/admin/dailyreport.php
+++ pages/admin/dailyreport.php
@@ -1,13 +1,15 @@
-
 <?php
 include('../connect.php');
-$data1="SELECT *
-FROM customerservice 
+include('adminhead.php');
+$currentdate = date('Y-m-d');
+$query = "SELECT *
+FROM booking b, customerservice cs
+WHERE b.csid = cs.csid AND b.assignedDate = '$currentdate'
 ";
 
-$query=mysqli_query($connection,$data1);
-$data1=mysqli_fetch_array($query);
-$dcount=mysqli_num_rows($query);
+$res = mysqli_query($connection, $query);
+$data1 = mysqli_fetch_array($res);
+$dcount = mysqli_num_rows($res);
 
 
 ?>
@@ -20,41 +22,88 @@ $dcount=mysqli_num_rows($query);
     <title>Document</title>
 </head>
 <body>
-    <?php
-        // if ($data < 1) 
-        // {
-        // echo "<p>No Booking Records Found for Today.</p>";
-        // echo "<script>window.location='booking.php'</script>";
-        // }
-        // else
-        // {
-            for($i=0;$i<$dcount;$i++) 
-        { 
-            $currentdate = date('Y-m-d');
-            
+<?php
+// if ($data < 1)
+// {
+// echo "<p>No Booking Records Found for Today.</p>";
+// echo "<script>window.location='booking.php'</script>";
+// }
+// else
+// {
 
-            $csid=$data1['csid'];
-            $csname=$data1['adminName'];
-            $data1=mysqli_fetch_array($query);
+// }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script type="text/javascript" src="js/jquery-3.1.1.slim.min.js"></script>
+    <script type="text/javascript" src="DataTables/datatables.min.js"></script>
+</head>
+<body>
 
-            $ok="SELECT distinct COUNT(b.bid) as bnum
+
+<div id="body-section">
+    <h1 class="form-title mt-5">Daily Report</h1>
+    <div class="table-container mb-5">
+        <table id="tableid" class="table ">
+            <thead>
+            <tr>
+                <!-- <th>#</th> -->
+
+                <th>Page Name</th>
+                <th>Booking Date</th>
+                <th>Bookings Count</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            for ($i = 0; $i < $dcount; $i++) {
+
+                $csid = $data1['csid'];
+                $csname = $data1['adminName'];
+                $pname = $data1['pageName'];
+                $data1 = mysqli_fetch_array($res);
+
+                $bookingCount = "SELECT distinct COUNT(b.bid) as bnum
             FROM  booking b, customerservice cs
-            WHERE b.csid = '$csid' and b.assignedDate = '$currentdate' and b.csid = cs.csid and b.status='Confirmed'
+            WHERE b.csid = '$csid' and b.assignedDate = '$currentdate' and b.csid = cs.csid
              ";
-             
-             $ret=mysqli_query($connection,$ok);
-             $show = mysqli_fetch_array($ret);
-           
-                ?>
-                <?php echo $csname?> has <?php echo $show['bnum']?> bookings tdy<br> 
-                <?php
-            
-               
-               
-            
-        }
-        // }
-    ?>
+
+                $ret = mysqli_query($connection, $bookingCount);
+                $show = mysqli_fetch_array($ret);
+                echo "<tr>";
+                echo "<td>" . $data1["pageName"] . "</td>";
+                echo "<td>" . $data1["date"] . "</td>";
+                echo "<td>" . $show["bnum"] . "</td>";
+                echo "</tr>";
+            }
+            ?>
+
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php
+include('../footer.php'); ?>
+
+<script>
+    $(document).ready(function () {
+        $('#tableid').DataTable({
+            order: [[0, 'asc']],
+            rowGroup: {
+                dataSrc: [0]
+            },
+            "columnDefs": [
+                {"width": "16%", "targets": -1}
+            ]
+        });
+    });
+</script>
 </body>
 </html>
 
